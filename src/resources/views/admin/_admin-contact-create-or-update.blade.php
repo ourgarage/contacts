ba92cf9d09cece7eb51078f1d0b22868bd5c2a17
Anonim-Khb <alvo4444@gmail.com>
2016-06-14 22:43:11 +1000

    ENG-25 In process

--- src/resources/views/admin/_admin-contact-create-or-update.blade.php
+++ src/resources/views/admin/_admin-contact-create-or-update.blade.php
@@ -19,7 +19,7 @@
           method="POST">
         {!! csrf_field() !!}
         <div class="form-group">
-            <textarea id="trumbowygEditor" name="text" class="form-control" rows="6"
+            <textarea id="contactCreate" name="text" class="form-control" rows="6"
                       placeholder="{{ trans('contacts::contacts.admin.textarea-placeholder') }}">{{ isset($contact) ? $contact->text : null }}</textarea>
         </div>
         <button class="btn btn-primary pull-right" type="submit">
@@ -31,16 +31,6 @@
 
 @section('js')
 
-    <script src="/libs/trumbowyg/dist/trumbowyg.min.js"></script>
-    <script>imageUploadUrl = "{!! route('contacts::admin::imageUpload') !!}";</script>
-
-    @if(App::getLocale() != 'en')
-        <script>siteLocale = '{!! App::getLocale() !!}';</script>
-        <script src="/libs/trumbowyg/dist/langs/{{ App::getLocale() }}.min.js"></script>
-    @endif
-
-    <script src="/libs/trumbowyg/dist/plugins/upload/trumbowyg.upload.js"></script>
-    <script src="/libs/trumbowyg/dist/plugins/colors/trumbowyg.colors.min.js"></script>
-    <script src="/js/trumbowyg/_upload-color-plugins.js"></script>
+    @include('api.wysiwyg', ['elementID' => '#contactCreate', 'siteLocale' => App::getLocale(), 'imageUploadUrl' => route('contacts::admin::imageUpload'), 'assembly' => 'full'])
 
 @endsection

52d7268974e8e897defc464a8a9050c9100b73bd
Anonim-Khb <alvo4444@gmail.com>
2016-08-14 20:07:06 +1000

    ENG-25 HZ

--- src/config/contacts.php
+++ src/config/contacts.php
@@ -1,12 +1,29 @@
 <?php
 
 return [
+
     'contacts' => [
+
         'name' => 'contacts',
-        'url' => 'contacts::admin::contactsIndex',
-        'caption' => 'Contacts',
-        'icon' => 'fa fa-map-signs',
-        'active' => 'adminContactsIndex',
-        'imageSavePath' => 'packages/contacts/images/'
+
+        'menu' => [
+            'url' => 'contacts::admin::contactsIndex',
+            'caption' => 'Contacts',
+            'icon' => 'fa fa-map-signs',
+            'active' => 'static-pages::admin::adminContactsIndex',
+        ],
+
+//        'menu-settings' => [
+//            'url' => 'static-pages::admin::get-settings',
+//            'caption' => 'Contacts pages settings',
+//            'icon' => 'fa fa-map-signs',
+//            'active' => 'static-pages::admin::get-settings',
+//        ],
+
+        'default-settings' => [
+            'meta-keywords' => 'Engin CMS, Laravel',
+            'meta-description' => 'This package for Engin CMS',
+            'meta-title' => 'Contacts package',
+        ],
     ],
 ];
